b0d3d0ab0dc73b68111f230dd26b5cb8333df37c
JinhoKim <rlawlsgh6306@gmail.com>
2021-03-06 12:54:31 +0900

    title modified

--- app.js
+++ app.js
@@ -1,25 +1,34 @@
 //jshint esversion:6
 
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
 
+const mongoose = require("mongoose");
+mongoose
+    .connect(
+        "mongodb+srv://camapData:kimyang@cluster0.dcygt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+        }
+    )
+    .then(() => console.log("MongoDB connected..."))
+    .catch((error) => console.log(error));
 
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://camapData:kimyang@cluster0.dcygt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
-    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
-}).then(() => console.log('MongoDB connected...'))
-.catch(error => console.log(error))
+app.use(express.static("public"));
 
-app.use(express.static('public'));
-
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
 });
 
-app.post('/', function(req, res) {
-    res.send('great');
+app.post("/", function (req, res) {
+    res.send("great");
 });
 
-app.listen(process.env.PORT || 3000, function() {
-    console.log('Server is running on port 3000.');
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Server is running on port 3000.");
 });
