06d2ef516dfca092a8cb6c60b4acc70bc15f2377
DaaEun <didekdms123@gmail.com>
2021-03-24 17:36:25 +0900

    Heroku 서버 D와 연동중

--- app.js
+++ app.js
@@ -5,24 +5,21 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var session = require("express-session");
 var logger = require("morgan");
-var models = require("./models/index.js");
+const models = require("./models/index.js");
 
 var indexRouter = require("./routes/index");
-// var usersRouter = require("./routes/users");
+var usersRouter = require("./routes/users");
 // var loginRouter = require("./routes/login");
 // var signupRouter = require("./routes/signup");
 
-var app = express();
+models.sequelize.sync().then( () => {
+  console.log(" DB 연결 성공");
+}).catch(err => {
+  console.log("연결 실패");
+  console.log(err);
+});
 
-// models.sequelize
-//     .sync()
-//     .then(() => {
-//         console.log("DB연결 성공");
-//     })
-//     .catch((err) => {
-//         console.log("연결실패");
-//         console.log(err);
-//     });
+var app = express();
 
 // views/layout.ejs를 기본 레이아웃으로 설정하고 <%- body %> 부분에 렌더링 된 html 문자열이 들어감
 app.set("layout", "layout");
@@ -37,22 +34,20 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(
-//     session({
-//         key: "sid",
-//         secret: "secret",
-//         resave: false,
-//         saveUninitialized: true,
-//         cookie: {
-//             maxAge: 24000 * 60 * 60,
-//         },
-//     })
-// );
+app.use(session({
+    key: 'sid',
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 24000 * 60 * 60 // 쿠키 유효기간 24시간
+    }
+  }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(expressLayouts);
 
 app.use("/", indexRouter);
-// app.use("/users", usersRouter);
+app.use("/user", usersRouter);
 // app.use("/signup", signupRouter);
 
 // catch 404 and forward to error handler
@@ -73,7 +68,3 @@ app.use(function (err, req, res, next) {
 
 module.exports = app;
 
-// const PORT = process.env.PORT || '8080';
-
-// //set the port
-// app.set("port", PORT);
