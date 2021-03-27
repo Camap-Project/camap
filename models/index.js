997ac9de3213af7cea1b8f3c7b2a70347998c4cd
DaaEun <didekdms123@gmail.com>
2021-03-27 16:17:28 +0900

    DB 연동 성공!

--- models/index.js
+++ models/index.js
@@ -57,9 +57,12 @@ fs
   .filter(file => {
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
-  .forEach(file => {
-    const model = sequelize['import'](path.join(__dirname, file));
-    db[model.name] = model;
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    )
+    db[model.name] = model
   });
 
 Object.keys(db).forEach(modelName => {
